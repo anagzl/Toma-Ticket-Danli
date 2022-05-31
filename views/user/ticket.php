90626911a844e55050d4fdc54ddd59def6ccd714
JBRIZO <60263601+JBRIZO@users.noreply.github.com>
2022-05-31 16:52:08 -0600

    Modificacion formato de ticket y base de datos
    Modificado formato para que contegna otro codigo qr con la direccion del portal de la institucion, anadido script de base de datos

--- views/user/ticket.php
+++ views/user/ticket.php
@@ -23,7 +23,7 @@ require('../assets/fpdf184/fpdf.php');
 include('../../assets/phpqrcode/qrlib.php');
 
 
-if(isset($_GET["idBitacora"])){
+if(isset($_POST["idBitacora"])){
     require('obtener_bitacora.php');
     $jsonObject = json_decode($json);
 }
@@ -38,11 +38,15 @@ $codigoDireccion = $jsonObject->siglas;
 // numero de ticket con zerofill
 $numeroTicketZeroFill = sprintf("%03d", $numeroTicket);
 
+//qr con direccion del portal
+QRcode::png("www.ip.gob.hn",'../../files/QR/QRPortal.png', QR_ECLEVEL_L, 3);
+
+
 //qr con id de bitacora
 QRcode::png("$idBitacora",'../../files/QR/QRTicket.png', QR_ECLEVEL_L, 3);
 
 //crear objeto FPDF
-$pdf= new FPDF('P','mm',array(80,80));
+$pdf= new FPDF('P','mm',array(80,90));
 $mid_x = $pdf->GetPageWidth() / 2;
 
 //titulo
@@ -54,30 +58,44 @@ $pdf->SetFont('Helvetica','B',20);
 //set up a page
 $pdf->AddPage('P');
 
+//titulo qr portal
+$pdf->SetFontSize(10);
+$pdf->Text(3, 5, "Portal:");
+//qr del portal
+$pdf->Image('../../files/QR/QRPortal.png',0,5,18,0);
+
+//titulo qr id
+$pdf->SetFontSize(10);
+$pdf->Text(62, 5, "ID:");
+
 //qr generado anteriormente con el id de la bitacora
-$pdf->Image('../../files/QR/QRTicket.png',60,5,15,0);
+$pdf->Image('../../files/QR/QRTicket.png',60,5,18,0);
 //logo centrado
-$pdf->Image('../../img/logoInstitucion/LOGO IP 3-01.png',20,5,40,0);
+$pdf->Image('../../img/logoInstitucion/LOGO NEGRO HORIZONTAL.png',4,3,75,18);
 
 //texto de bienvenido
-$pdf->SetFontSize(10);
-$pdf->Text($mid_x - ($pdf->GetStringWidth("Bienvenido $nombreUsuario") / 2), 22, "Bienvenido $nombreUsuario");
+$pdf->SetFontSize(15);
+$pdf->Text($mid_x - ($pdf->GetStringWidth("Bienvenido") / 2), 27, "Bienvenido");
 
 // sede
-$pdf->Text($mid_x - ($pdf->GetStringWidth("$jsonObject->siglas_ubicacion / $jsonObject->nombre_departamento") / 2), 30, "$jsonObject->siglas_ubicacion / $jsonObject->nombre_departamento");
+$pdf->Text($mid_x - ($pdf->GetStringWidth("$jsonObject->siglas_ubicacion / $jsonObject->nombre_departamento") / 2), 34, "$jsonObject->siglas_ubicacion / $jsonObject->nombre_departamento");
+
+//nombre
+$pdf->Text($mid_x - ($pdf->GetStringWidth("$nombreUsuario") / 2), 42, "$nombreUsuario");
+
 
 // numero de ticket
-$pdf->SetFontSize(60);
-$pdf->Text($mid_x - ($pdf->GetStringWidth("$codigoDireccion$numeroTicketZeroFill") / 2), 50, "$codigoDireccion$numeroTicketZeroFill");
+$pdf->SetFontSize(65);
+$pdf->Text($mid_x - ($pdf->GetStringWidth("$codigoDireccion$numeroTicketZeroFill") / 2), 62, "$codigoDireccion$numeroTicketZeroFill");
 
 // hora
-$pdf->SetFontSize(10);
-$pdf->Text($mid_x - ($pdf->GetStringWidth("$fechaHora") / 2), 57, "$fechaHora");
+$pdf->SetFontSize(12);
+$pdf->Text($mid_x - ($pdf->GetStringWidth("$fechaHora") / 2), 68, "$fechaHora");
 // pagina web
-$pdf->Text($mid_x - ($pdf->GetStringWidth("www.ip.gob.hn") / 2), 63, "www.ip.gob.hn");
+$pdf->Text($mid_x - ($pdf->GetStringWidth("www.ip.gob.hn") / 2), 74, "www.ip.gob.hn");
 // disclaimer
 $pdf->SetFontSize(8);
-$pdf->Text($mid_x - ($pdf->GetStringWidth("Tickets pueden no ser llamados en secuencia") / 2), 68, "Ticket pueden no ser llamados en secuencia");
+$pdf->Text($mid_x - ($pdf->GetStringWidth("Tickets pueden no ser llamados en secuencia") / 2), 78, "Ticket pueden no ser llamados en secuencia");
 
 
 
