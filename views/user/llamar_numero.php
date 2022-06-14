88c3b18ca8784023dfbe0e4f73e12f9e6b135d7c
JBRIZO <60263601+JBRIZO@users.noreply.github.com>
2022-06-14 15:10:31 -0600

    Avance en historia de reproduccion de numero de ticket
    Cambio de nombre en los archivos de voz para la reproduccion de audio

--- views/user/llamar_numero.php
+++ views/user/llamar_numero.php
@@ -9,15 +9,40 @@
 */
 
 // Este es un ejemplo
-$numero = 300;
+$numero = 15;
 $arrayNumeros = str_split($numero);
 
 //crear arreglo
-echo "var sonidos = new Array();";
-
-for($i = 0; $i < count($arrayNumeros); $i++){
-    echo "sonidos.push(new Audio('../../files/Voz/$arrayNumeros[$i].mp3'));";
+if ($numero < 20){
+    echo "<script>var sonidos = new Array();
+    sonidos.push(new Audio('../../files/Voz/Número.mp3'));
+    sonidos.push(new Audio('../../files/Voz/$numero.mp3'));
+";
+}else{
+    if($numero < 100 && $numero > 19){
+        echo "<script>var sonidos = new Array();
+        sonidos.push(new Audio('../../files/Voz/Número.mp3'));
+        sonidos.push(new Audio('../../files/Voz/$arrayNumeros[0]0.mp3'));
+        sonidos.push(new Audio('../../files/Voz/y.mp3'));
+        sonidos.push(new Audio('../../files/Voz/$arrayNumeros[1].mp3'));";
+    }else{
+        echo "<script>var sonidos = new Array();
+        sonidos.push(new Audio('../../files/Voz/Número.mp3'));
+        sonidos.push(new Audio('../../files/Voz/$arrayNumeros[0]00.mp3'));
+        sonidos.push(new Audio('../../files/Voz/$arrayNumeros[1]0.mp3'));
+        sonidos.push(new Audio('../../files/Voz/y.mp3'));
+        sonidos.push(new Audio('../../files/Voz/$arrayNumeros[2].mp3'));
+        ";
+    }
 }
+    
+
+
+
+
+// for($i = 0; $i < count($arrayNumeros); $i++){
+//     echo "sonidos.push(new Audio('../../files/Voz/$arrayNumeros[$i]00.mp3'));";
+// }
 
 //reproducir audios secuencialmente
 echo "
@@ -28,6 +53,6 @@ function playSnd() {
     if (i == sonidos.length) return;
     sonidos[i].addEventListener('ended', playSnd);
     sonidos[i].play();
-}";
+}</script>";
 
 ?>
