34c4e58cb4ad760d6ea03017d15ec6417ec01395
AnaZav <Gissela238@gmail.com>
2022-06-20 15:59:26 -0600

    avance en crear función para limpiar el almacenamiento de datos

---
+++ controllers/tb_limpiar_almacenamiento.js
@@ -0,0 +1,15 @@
+
+/**
+ * Funcion Global para guardar datos de session
+ * 
+ * @Autor:Ana Zavala
+ * @Fecha Creacion: 20/06/2022
+ 
+*/
+
+//estos datos se eliminaran una vez que se cierra el navegador
+function limpiar_almacenamiento() {
+
+  sessionStorage.removeItem('nombre');
+              
+            }
