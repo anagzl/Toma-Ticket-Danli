2f60c721335357d82ac27ef1a6f3225bf414e85e
AnaZav <Gissela238@gmail.com>
2022-06-24 12:31:10 -0600

    crear usuario solo id en tabla usuario
    *imprimir ticket sin llenar datos

--- controllers/tb_ingreso_solo_id.js
+++ controllers/tb_ingreso_solo_id.js
@@ -1,23 +1,27 @@
   
 /**autor: Ana Zavala
-* Funcionalidad de Crear un registro nuevo
-*modificacación 23/06/2022  
+* Funcionalidad de Crear un registro nuevo solo ingresando id
+*fecha_creacion 24/06/2022  
 */
-
-function registrar_sin_id(){
+function registrar_solo_id(){
 
               $(document).on('submit','#formularioCreacioningreso_cliente',function(event){
                   event.preventDefault();
                      var idUsuario                   = $("#idUsuario").val();
-                
-     
+                     var primerNombre                = NULL;
+                     var segundoNombre               = NULL;
+                     var primerApellido              = NULL;
+                     var segundoApellido             = NULL;
+                     var numeroCelular               = NULL;
+                     var correo                      = NULL;
+
                      let formData = new FormData(this)
                      formData.append('idUsuario', idUsuario);
      
-                 /* Validar campos que no lo envien v*/
-                     if(idUsuario != ''){
+                 /* Validar campos que no lo envien */
+                 if(idUsuario != '' && primerNombre != '' && segundoNombre != ''&& primerApellido != '' && segundoApellido != '' && numeroCelular != ''&& correo != '' ){
                          $.ajax({
-                             url:"datos_cliente.php",
+                             url:"crear_ingreso_solo_id.php",
                                  method:'POST',
                                  data:formData,
                                  contentType:false,
@@ -40,6 +44,6 @@ function registrar_sin_id(){
                          alert("Algunos campos son obligatorios.");
                      }
                  });
-                  $.get(`ticket_para_prueba.php?idTicket=2&direccion=1`,function(data,status){});
-                 window.open('imprimir_ticket.php', '_blank', 'location=yes,height=570,width=520,scrollbars=yes,status=yes'); 
+              /*  $.get(`ticket_para_prueba.php?idTicket=2&direccion=1`,function(data,status){});
+                 window.open('imprimir_ticket.php', '_blank', 'location=yes,height=570,width=520,scrollbars=yes,status=yes'); */
                  }
