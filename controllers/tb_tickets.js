e76e8f069d296b1631ddb3f9b33e7907ca47a8d4
JBRIZO <60263601+JBRIZO@users.noreply.github.com>
2022-07-08 16:30:48 -0600

    Terminada funcionalidad para visualizar tickets en cola en tiempo real
    El administrador puede visualizar tickets en cola para cada dieccion y habilitarlos o deshabilitarlos

---
+++ controllers/tb_tickets.js
@@ -0,0 +1,83 @@
+/**
+ * Funcionalidad de llenar la tabla de datos
+ */
+$(document).ready(function(){
+    var direccion = $("#direccion").val();
+    cargar_datatable_direccion(direccion);
+});
+
+var dataTable;
+
+$("#direccion").change(function(){
+    var direccionSeleccionada = $("#direccion").val();
+    dataTable.destroy();
+    cargar_datatable_direccion(direccionSeleccionada);
+});
+
+function cargar_datatable_direccion(direccion){
+    dataTable = $('#datos_tickets').DataTable({
+        "paging":false,
+        "searching":false,
+        "processing":true,
+        "serverSide":true,
+        "defaultContent":  "<i>Not set</i>",
+        "order":[],
+        "ajax":{
+                url:"obtener_registros_tickets.php",
+                type:"POST",
+                data: {direccion : direccion}
+                },
+                 "columnsDefs":[
+                    {
+                        "targets":[0,3,4],
+                        "orderable":false, 
+                    },
+                ],
+        "language": {
+            "decimal": "",
+            "emptyTable": "<i class='bi bi-emoji-frown'></i>  No hay registros encontrados",
+            "info": "<i class='bi bi-eye-fill'></i> Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+            "infoEmpty": "<i class='bi bi-menu-button-fill'></i>  Mostrando 0 a 0 de 0 Entradas",
+            "infoFiltered": "(<i class='bi bi-funnel-fill'></i> Filtrado de _MAX_ total entradas)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "<i class='bi bi-menu-button-fill'></i> Mostrar _MENU_ Entradas",
+            "loadingRecords": "<div class='spinner-border text-info' role='status'><span class='visually-hidden'>Loading...</span> </div> Cargando...",
+            "processing": "<div class='spinner-border text-info' role='status'><span class='visually-hidden'>Loading...</span> </div> Procesando...",
+            "search": "<i class='bi bi-search'></i> Buscar:",
+            "zeroRecords": "<i class='bi bi-emoji-frown'></i> Sin resultados encontrados",
+            "paginate": {
+                "first": "<i class='bi bi-skip-backward-fill'></i> Primero",
+                "last": "<i class='bi bi-skip-forward-fill'></i> Ultimo",
+                "next": "Siguiente <i class='bi bi-caret-right'></i>",
+                "previous": "<i class='bi bi-caret-left'></i> Anterior"
+            }
+        } 
+    }); 
+}
+
+ //Funcionalidad para deshabilitar o borrar ticket
+ $(document).on('click', '.borrar', function(){
+    var idTicket = $(this).attr("id");
+    var direccion = $('#direccion').val();
+    if(confirm("¿Seguro que quiere habilitar/deshabilitar este ticket?: " + idTicket))
+    {
+        $.ajax({
+            url:"cambiar_estado_ticket.php",
+            method:"POST",
+            data:{idTicket:idTicket,
+                  direccion:direccion},
+            success:function(data)
+            {
+                alert(data);
+                dataTable.ajax.reload();
+            }
+        });
+    }
+    else
+    {
+        return false;
+    }
+});
+
+  
---
+++ views/admin/tickets.php
@@ -0,0 +1,94 @@
+<!-- Llamado de la cabeza del -->
+<?php
+require 'header.php';
+?>
+
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+    <!-- Page Heading -->
+    <h1 class="h3 mb-2 text-gray-800"><i class="bi bi-clipboard-fill"></i>  Tablero de Tickets </h1>
+        <p class="mb-4">Área donde se puede visualizar la cola de tickets en tiempo real. Nota: Solo se podran visualizar los tickets del dia de hoy, a los 00:00 se reiniciara el contador, los tickets de hoy no seran validos el dia de mañana para ninguna operación.</p>
+            <!-- DataTales Example -->
+            <div class="card shadow mb-4">
+                <div class="card-body"> 
+                    <!-- Container Fluid -->
+                    <div class="container-fluid">
+                        <!-- Page Heading -->
+                            <div class="card-header py-3">
+                                <h6 class="m-0 font-weight-bold text-primary"><i class="bi bi-calendar4-week"></i> &nbsp; Registro de Tickets</h6>
+                            </div>
+                            <div class="card-body">
+                                <div class="row text-center">
+                                    <div class="col-1">
+                                        <label class="form-select~label align-middle" for="direccion">Dirección:</label>
+                                    </div>
+                                    <div class="col-4">
+                                        <select class="form-select align-middle" name="direccion" id="direccion">
+                                            <?php
+                                                include("../../config/conexion.php");
+                                                $query = $conexion->prepare("SELECT idDireccion,
+                                                                                    nombre
+                                                                                    FROM direccion");
+                                                $query->execute();
+                                                $data = $query->fetchAll();
+
+                                                foreach ($data as $valores):
+                                                echo '<option value="'.$valores["idDireccion"].'">'.$valores["nombre"].'</option>';
+                                                endforeach;
+                                            ?>
+                                        </select>
+                                    </div>
+                                </div>
+                                </div>
+                                    <div class="table-responsive"><!-- inicio de table responsive -->
+                                            <!-- Tabla  -->
+                                            <table id="datos_tickets" class="table table-hover">
+                                                <!-- Encabezado -->
+                                                <thead ><!-- class="table-dark" -->
+                                                    <!-- Creacion de fila de encabezados -->
+                                                    <tr>
+                                                        <!-- Columnas -->
+                                                        <th><i class="bi bi-hash"></i>  Número </th>
+                                                        <th><i class="bi bi-hash"></i>  Ticket </th>
+                                                        <th><i class="bi bi-list-check"></i> Trámite </th>
+                                                        <th><i class="bi bi-shop-window"></i> Ventanilla</th>
+                                                        <th><i class="bi bi-toggles"></i> Estado</th>
+                                                    </tr>
+                                                </thead>
+                                            </table>
+                                    </div><!-- fin de table responsive -->
+                            </div>
+                        </div>
+                    </div>
+
+                            <!-- /.container-fluid -->  
+
+               
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+<!-- End of Main Content -->
+
+    <!--  JavaScript -->
+    <!-- Jquery  -->
+    <script src="../../assets/jquery-3.6.0/jquery-3.6.0.min.js"></script>
+
+    <!-- Data tables -->
+    <script charset="utf8"  src="../../assets/datatables/js/jquery.dataTables.min.js" ></script>
+    <!--     <script src="//cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script> -->
+
+    <!--  Bootstrap  -->
+    <script src="../../assets/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../../assets/bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
+
+
+    <!--  Controlador para ver asistencia  -->
+    <script src="../../controllers/tb_tickets.js"></script>
+
+<!-- Llamdo del pie del documento -->
+<?php
+require 'footer.php';
+?>
