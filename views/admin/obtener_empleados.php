a740d3dc31fdf8d5eec3e949be13ea8d511fce54
AnaZav <Gissela238@gmail.com>
2022-07-09 01:07:56 -0500

    vista obtener empleado

--- views/admin/obtener_empleados.php
+++ views/admin/obtener_empleados.php
@@ -30,15 +30,13 @@
                   $query .=' OR Rol_idRol LIKE "%'.$_POST["search"]["value"].'%"';
 
                 /* Filtar por ventanilla */
-               /*   $query .=' OR Ventanilla_idVentanilla LIKE "%'.$_POST["search"]["value"].'%"'; */
+                 $query .=' OR Ventanilla_idVentanilla LIKE "%'.$_POST["search"]["value"].'%"';
 
                 /* Filtar por correo Institucional */
                 $query .=' OR correoInstitucional LIKE "%'.$_POST["search"]["value"].'%"';
                 
                 /* Filtar por login */
                      $query .=' OR login LIKE "%'.$_POST["search"]["value"].'%"';
-
-               
             }
 
 
@@ -74,35 +72,40 @@
                 $sub_array[]=$fila["idEmpleado"];
                 $sub_array[]=$fila["Usuario_idUsuario"]; 
                 $sub_array[]=$fila["Rol_idRol"];
-           /*   $sub_array[]=$fila["Ventanilla_idVentanilla"];    */
+                $sub_array[]=$fila["Ventanilla_idVentanilla"];   
                 $sub_array[]=$fila["correoInstitucional"];
-                $sub_array[]=$fila["login"];
-             
-              
+                $sub_array[]=$fila["login"];            
+
                 /* Funcionalidad para editar */
                 $sub_array[]='<button type="button" name="editar" id="'.$fila["idEmpleado"].'" class="btn btn-warning btn-xs editar"><i class="bi bi-pencil-square"></i> Editar </button>';
-              
+            
+               /*  if($fila["estado"]== 1 )
+                {
+                    $sub_array[]='<button type="button"name="borrar" id="'.$fila["idEmpleado"].'" class="btn btn-success borrar"><i class="bi bi-toggle-on"></i> Habilitado</button>';
+                }
+                else
+            {
+                $sub_array[]='<button type="button"name="borrar" id="'.$fila["idEmpleado"].'" class="btn btn-danger borrar"><i  class="bi bi-toggle-off"></i> Deshabilitado </button>';
+        
+            } */
+                    $datos[] = $sub_array;
 
-
-           if($fila["Estado"]== 1){
-                    $sub_array[]='<button type="button"name="cambiar" id="'.$fila["idEmpleado"].'" class="btn btn-success borrar"><i class="bi bi-toggle-on"></i> Habilitado</button>';
-                }else{
-                    $sub_array[]='<button type="button"name="cambiar" id="'.$fila["idEmpleado"].'" class="btn btn-danger borrar"><i  class="bi bi-toggle-off"></i> Deshabilitado </button>';
-                }  
-                $datos[] = $sub_array;
-    
             }
-    
+
     /**
      * Funcionalidad de la ejecucion de todos los regitros para la salida 
      */    
-    
+            
+        /* Validar que no este vacio el resultado */
+        
+
             $salida = array(
                 "draw"               => intval($_POST["draw"]),
                 "recordsTotal"       => $filtered_rows,
                 "recordsFiltered"    => obtener_todos_registros_empleado(),
                 "data"               => $datos
             );
-    
+
             /* Mando los datos en formato json */
             echo json_encode($salida);
+    ?>
