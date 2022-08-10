fb4aefccdb056144eec447cd95df7fcdf2d89d9a
Luis_Estrada <lestradaeu@gmail.com>
2022-08-10 00:11:42 -0600

    Archivo de la BD Tomaticket generado el 09-08-2022
    *Configuracion de la conexion sin ldap
    *BD generada de produccion 9-8-2022

--- config/control_ldap.php
+++ config/control_ldap.php
@@ -1,15 +1,23 @@
 <?php
 		date_default_timezone_set('America/Tegucigalpa');
+		require("ldap.php");
+		header("Content-Type: text/html; charset=utf-8");
 		$usr = $_POST["usuario"];
-		include("conexion.php");
-		require_once("../views/user/obtener_empleado.php");
-		$datosUsuario = obtener_empleado_usuario($usr,$conexion);
-		session_start();
-		$_SESSION["user"] = $usuario;
-		$_SESSION["login"] = $usr;
-		$_SESSION["autentica"] = "SIP";
-		if(empty($datosUsuario)){
-				echo"<script> alert('Ese usuario no esta registrado en este sistema.'); window.location.href='../views/user/login.php'; </script>";
+		$usuario = mailboxpowerloginrd($usr,$_POST["clave"]);
+		if($usuario == "0" || $usuario == ''){
+			$_SERVER = array();
+			$_SESSION = array();
+			echo"<script> alert('Usuario o clave incorrecta. Vuelva a digitarlos por favor.'); window.location.href='../views/user/login.php'; </script>";
+		}else{
+			include("conexion.php");
+			require_once("../views/user/obtener_empleado.php");
+			$datosUsuario = obtener_empleado_usuario($usr,$conexion);
+			session_start();
+			$_SESSION["user"] = $usuario;
+			$_SESSION["login"] = $usr;
+			$_SESSION["autentica"] = "SIP";
+			if(empty($datosUsuario) || $datosUsuario['estado'] == 0){
+				echo"<script> alert('Ese usuario no esta registrado en este sistema o ha sido desactivado.'); window.location.href='../views/user/login.php'; </script>";
 			}
 
 			if($datosUsuario['Rol_idRol'] == '1'){
@@ -34,5 +42,7 @@
 				}
 			}else{
 				echo"<script> alert('Ese usuario no esta registrado en este sistema.'); window.location.href='../views/user/login.php'; </script>";
-			}	
+			}
+
+		}
 ?>
